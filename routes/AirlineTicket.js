43c82e3d7351bc3061d2e5747d9f935ee3fb74ad
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-10-30 11:31:09 -0500

    part 5

--- routes/AirlineTicket.js
+++ routes/AirlineTicket.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results AirlineTicket' });
+  res.render('AirlineTicket', { title: 'Search Results AirlineTicket' });
 });
 
 module.exports = router;

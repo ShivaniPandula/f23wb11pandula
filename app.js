0ff9dc93d5d012ba8a7c918d583358ea11fbc7c4
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-10-30 11:53:27 -0500

    part 7

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var AirlineTicketRouter = require('./routes/AirlineTicket');
+var gridbuildRouter = require('./routes/board');
+
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/AirlineTicket', AirlineTicketRouter);
+app.use('/board', gridbuildRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
